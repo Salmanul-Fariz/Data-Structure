a6a77d76a977bdb41586635ce9583ed5d080fccd
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-15 19:03:29 +0530

    Filter Array

--- array.js
+++ array.js
@@ -10,3 +10,16 @@ console.log('Map Array');
 console.log('------------------------------');
 console.log(resultMap);
 // Big-O = O(n)
+
+///////////////////////////////
+//////// Filter Array /////////
+///////////////////////////////
+
+const filterArray = [1, 2, 3, 4, 5];
+const resultFilter = filterArray.filter((e) => e > 2);
+
+console.log('------------------------------');
+console.log('Filter Array');
+console.log('------------------------------');
+console.log(resultFilter);
+// Big-O = O(n)
