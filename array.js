bc03b7fa1f685940183ba650c18e383caa52ffca
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-15 19:01:00 +0530

    Map Array

---
+++ array.js
@@ -0,0 +1,12 @@
+///////////////////////////////
+///////// Map Array ///////////
+///////////////////////////////
+
+const mapArray = [1, 2, 3, 4, 5];
+const resultMap = mapArray.map((e) => e + 1);
+
+console.log('------------------------------');
+console.log('Map Array');
+console.log('------------------------------');
+console.log(resultMap);
+// Big-O = O(n)
