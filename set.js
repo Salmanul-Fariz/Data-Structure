82f6da655812c8e24c8fc2c6dc6a3160f337c01d
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-16 13:23:08 +0530

    Sample of Set

---
+++ set.js
@@ -0,0 +1,10 @@
+///////////////////////////////
+///////// Sample Set //////////
+///////////////////////////////
+
+const sampleSet = new Set([1, 2, 3]);
+
+console.log('------------------------------');
+console.log('Sample Set');
+console.log('------------------------------');
+console.log(sampleSet);
