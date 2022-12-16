7942bbe1cf0c12a52d62c29ee011d3bdfbf97415
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-16 13:24:37 +0530

    Add Element Set

--- set.js
+++ set.js
@@ -8,3 +8,15 @@ console.log('------------------------------');
 console.log('Sample Set');
 console.log('------------------------------');
 console.log(sampleSet);
+
+///////////////////////////////
+///////// Add El Set //////////
+///////////////////////////////
+
+const addElSet = new Set([1, 2, 3]);
+addElSet.add(4);
+
+console.log('------------------------------');
+console.log('Add Element Set');
+console.log('------------------------------');
+console.log(addElSet);
