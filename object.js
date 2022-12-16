cb34dc2ed2f775c3e38274a1023cbfa08aea2aa2
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-16 11:02:29 +0530

    Values of Object

--- object.js
+++ object.js
@@ -13,3 +13,19 @@ console.log('Keys of Object');
 console.log('------------------------------');
 console.log(Object.keys(keysOfObject));
 // Big-O = O(n)
+
+///////////////////////////////
+////// Values of Object ///////
+///////////////////////////////
+
+const valuesOfObject = {
+  name: 'user',
+  age: 20,
+  country: 'india',
+};
+
+console.log('------------------------------');
+console.log('Values of Object');
+console.log('------------------------------');
+console.log(Object.values(valuesOfObject));
+// Big-O = O(n)
