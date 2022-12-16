f31d76d6e5e5fd1c46a0b7d1b2c1636eef877074
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-16 10:58:44 +0530

    Keys of Object

---
+++ object.js
@@ -0,0 +1,15 @@
+///////////////////////////////
+////// Keys of Object /////////
+///////////////////////////////
+
+const keysOfObject = {
+  name: 'user',
+  age: 20,
+  country: 'india',
+};
+
+console.log('------------------------------');
+console.log('Keys of Object');
+console.log('------------------------------');
+console.log(Object.keys(keysOfObject));
+// Big-O = O(n)
