e3f9976f1824584fc2fe58cc8aa5e1edeb7e2445
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-24 17:06:13 +0530

    optimized queue initialized

---
+++ optimisedQueue.js
@@ -0,0 +1,35 @@
+class Queue {
+  constructor() {
+    this.items = {};
+    this.rear = 0;
+    this.front = 0;
+  }
+
+  enqueue(el) {
+    this.items[this.rear] = el;
+    this.rear++;
+  }
+
+  dequeue() {
+    const item = this.items[this.front];
+    delete this.items[this.front];
+    this.front++;
+    return item;
+  }
+
+  isEmpty() {
+    return this.items[this.front] - this.items[this.rear] === 0;
+  }
+
+  peek() {
+    return this.items[this.front];
+  }
+
+  size() {
+    return this.items[this.rear] - this.items[this.front];
+  }
+
+  print() {
+    console.log(this.items);
+  }
+}
