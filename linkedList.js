0ac2f7229ffdfa12067eb4b82023c047e9db26ea
Salmanul Fariz N <salmanulfariz997@gmail.com>
2023-03-01 22:03:04 +0530

    completed the Search methord in linked list

--- linkedList.js
+++ linkedList.js
@@ -68,6 +68,73 @@ class LinkedList {
     }
   }
 
+  removeFrom(index) {
+    if (index < 0 || index >= this.size) {
+      return null;
+    }
+
+    let removeNode;
+    if (index === 0) {
+      removeNode = this.head;
+      this.head = this.head.next;
+    } else {
+      let prev = this.head;
+      for (let i = 0; i < index - 1; i++) {
+        prev = prev.next;
+      }
+
+      removeNode = prev.next;
+      prev.next = removeNode.next;
+    }
+
+    this.size--;
+    return removeNode;
+  }
+
+  removeValue(val) {
+    if (this.isEmpty()) {
+      return null;
+    }
+
+    if (this.head.value === val) {
+      this.head = this.head.next;
+      this.size--;
+      return val;
+    } else {
+      let prev = this.head;
+      while (prev.next && prev.next.value !== val) {
+        prev = prev.next;
+      }
+
+      if (prev.next) {
+        const removeNode = prev.next;
+        prev.next = removeNode.next;
+        this.size--;
+        return val;
+      }
+
+      return null;
+    }
+  }
+
+  search(val) {
+    if (this.isEmpty()) {
+      return -1;
+    }
+
+    let index = 0;
+    let curr = this.head;
+
+    while (curr) {
+      if (curr.value === val) {
+        return index;
+      }
+      curr = curr.next;
+      index++;
+    }
+    return -1;
+  }
+
   print() {
     if (this.isEmpty()) {
       console.log('Empty');
@@ -92,7 +159,9 @@ list.append(1);
 list.append(2);
 list.append(3);
 list.append(4);
-console.log(list.size);
-list.insert(5, 2);
-
+// console.log(list.size);
+list.insert(5, 4);
+// list.removeFrom(3);
+// list.removeValue(5);
 list.print();
+console.log(list.search(6));
